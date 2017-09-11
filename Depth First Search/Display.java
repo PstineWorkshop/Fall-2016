d151c37165323727cc0e2bc935177d6cf806d450
PstineWorkshop <31455515+PstineWorkshop@users.noreply.github.com>
2017-09-11 15:19:31 -0500

    Add files via upload

---
+++ Depth First Search/Display.java
@@ -0,0 +1,72 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+
+public class Display extends JPanel implements ActionListener
+{
+   DMaze  m;
+   Timer timer;
+   
+   public Display()
+   {
+      timer = new Timer(100, this);
+      timer.start();       
+   }
+   
+   public void setDMaze(DMaze d)
+   {
+      m = d;
+   }
+   
+   public void actionPerformed(ActionEvent e) 
+   {
+      repaint();
+   }
+
+   public void paint (Graphics g)
+   {
+      super.paint(g);
+      if( m == null || m.numRows == 0 || m.numCols == 0) 
+      {
+         return;
+      }
+      int hStep = getHeight() / m.numRows;
+      int wStep = getWidth() / m.numCols;
+
+      for( int i=0; i<m.numRows; i++ )
+      {
+         for( int j=0; j<m.numCols; j++ )
+         {
+            if( m.grid[i][j] == m.WALL )
+            {
+               Color c = new Color( 100, 50, 255 );
+               g.setColor(c);
+               g.fillRect(wStep * j + 1, hStep * i + 1, wStep-2, hStep-2);
+            }          
+            else if( m.grid[i][j] == m.START )
+            {
+               Color c = new Color( 100, 255, 100 );
+               g.setColor(c);
+               g.fillRect(wStep * j + 1, hStep * i + 1, wStep-2, hStep-2);
+            }          
+            else if( m.grid[i][j] == m.END )
+            {
+               Color c = new Color( 255, 100, 100 );
+               g.setColor(c);
+               g.fillRect(wStep * j + 1, hStep * i + 1, wStep-2, hStep-2);
+            }          
+            else if( m.visited[i][j] == true )
+            {
+               Color c = new Color( 150, 150, 255 );
+               g.setColor(c);
+               g.fillOval(wStep * j + 4, hStep * i + 4, wStep-8, hStep-8);
+            }          
+         }
+      }
+   } 
+}
